8b6fa17c43ba343f3ff5a6fb53dd809a4015ae6f
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2019-12-21 22:22:20 +0900

    Visualization dataset/make_low_rank_matrix.py dataset/make_hastie_10_2.py

--- dataset/make_hastie_10_2.py
+++ dataset/make_hastie_10_2.py
@@ -1,7 +1,13 @@
 from sklearn import datasets
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # make_hastie_10_2 data
 X,y = datasets.make_hastie_10_2(n_samples=100,random_state=None)
 print(X)
 print(y)
+
+dataframe = pd.DataFrame(data=X)
+datalabel = pd.Series(data=y)
+gr = pd.plotting.scatter_matrix(dataframe,c=datalabel,figsize=(12,12))
+plt.show()
--- dataset/make_low_rank_matrix.py
+++ dataset/make_low_rank_matrix.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 # make_low_rank_matrix data
 X = datasets.make_low_rank_matrix(n_samples=100,n_features=2,effective_rank=2,tail_strength=0.5,random_state=None)
 print(X)
+
+plt.scatter(X[:,0],X[:,1])
+plt.show()
