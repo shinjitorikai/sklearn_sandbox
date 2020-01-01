3292d16a97ab03bb7c65e3bc3d81e4c74e89929c
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-01-01 10:49:40 +0900

    Modify regression/linear_models/ridge_regression.py  - add RigdeCV()

--- regression/linear_models/ridge_regression.py
+++ regression/linear_models/ridge_regression.py
@@ -1,4 +1,5 @@
 from sklearn import linear_model
+import numpy as np
 
 # data for fit
 X = [[0,0],[0,0],[1,1]]
@@ -8,6 +9,13 @@ y = [0.0,0.1,1.0]
 model = linear_model.Ridge(alpha=0.5)
 model.fit(X,y)
 
+# setting the regularization parameter(generalized cross-validation)
+model2 = linear_model.RidgeCV(alphas=np.logspace(start=-6,stop=6,num=13))
+model2.fit(X,y)
+
 # print result(coefficient/intercept)
 print(model.coef_)
 print(model.intercept_)
+
+# print result(alpha)
+print(model2.alpha_)
