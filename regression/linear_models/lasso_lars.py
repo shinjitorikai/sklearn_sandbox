9968bffa08328f5ba2f1c2ccf8510c487c5968ad
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-01-03 19:41:05 +0900

    Add regression/linear_models/lasso_lars.py

---
+++ regression/linear_models/lasso_lars.py
@@ -0,0 +1,17 @@
+from sklearn import linear_model
+import numpy as np
+
+# data for fit
+X = [[0,0],[1,1]]
+y = [0,1]
+
+# fit by Lasso(LARS) Regression
+model = linear_model.LassoLars(alpha=0.1)
+model.fit(X,y)
+
+# print result(coefficient/intercept)
+print(model.coef_)
+print(model.intercept_)
+
+# estimate
+print(model.predict([[1,1]]))
