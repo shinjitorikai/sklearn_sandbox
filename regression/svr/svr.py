51d2e4e1fd20cc346dfabff199a162554cb54f1c
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-01-06 00:03:45 +0900

    Modify regression/svr/svr.py  - kernel type selectable

--- regression/svr/svr.py
+++ regression/svr/svr.py
@@ -5,7 +5,11 @@ X = [[0,0], [2,2]]
 y = [0.5, 2.5]
 
 # fit
-model = svm.SVR()
+#model = svm.SVR()
+model = svm.SVR(kernel='rbf')
+#model = svm.SVR(kernel='linear')
+#model = svm.SVR(kernel='sigmoid')
+print(model.kernel)
 model.fit(X,y)
 
 # predict
