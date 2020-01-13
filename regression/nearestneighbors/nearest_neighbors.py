ec8096f27fac1c5d16d4b4f4c73067ca1f29f428
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-01-13 22:37:53 +0900

    Modify regression/nearestneighbors/nearest_neighbors.py - add model parameter section

--- regression/nearestneighbors/nearest_neighbors.py
+++ regression/nearestneighbors/nearest_neighbors.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import neighbors
 
+# model parameter
+#  - weights
+#param_weights = 'uniform'
+param_weights = 'distance'
+#  - n_neighbors
+param_n_neibors = 5
+
 # data for fit
 np.random.seed(0)
 X = np.sort(5*np.random.rand(40,1),axis=0)
@@ -9,8 +16,7 @@ T = np.linspace(0,5,500)[:,np.newaxis]
 y = np.sin(X).ravel()
 
 # fit / predict
-n_neibors = 5
-model = neighbors.KNeighborsRegressor(n_neighbors=n_neibors,weights='uniform')
+model = neighbors.KNeighborsRegressor(n_neighbors=param_n_neibors,weights=param_weights)
 y_ = model.fit(X,y).predict(T)
 
 # plot result
